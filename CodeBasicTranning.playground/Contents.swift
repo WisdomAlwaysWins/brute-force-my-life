e16504a2ba4227fba9722588c7c64c6efe272edd
J <knowisdom314@gmail.com>
2026-01-14 13:26:19 +0900

    프로그래머스: 정수 찾기

--- CodeBasicTranning.playground/Contents.swift
+++ CodeBasicTranning.playground/Contents.swift
@@ -113,3 +113,17 @@ func solution8(_ myString:String, _ pat:String) -> Int {
 
 print(solution8("ABBAA", "AABB"))
 print(solution8("ABAB", "ABAB"))
+
+/*
+ 정수 찾기
+ 
+ 정수 리스트 num_list와 찾으려는 정수 n이 주어질 때, num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+*/
+
+func solution9(_ num_list:[Int], _ n:Int) -> Int {
+    return num_list.contains(n) ? 1 : 0
+}
+
+print(solution9([1, 2, 3, 4, 5], 3)) // 1
+print(solution9([15, 98, 23, 2, 15], 20)) // 0
+
