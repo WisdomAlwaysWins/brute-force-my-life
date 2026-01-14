03e3c5516e76953da9cf11a2729236e37364d4c0
J <knowisdom314@gmail.com>
2026-01-14 11:58:51 +0900

    프로그래머스: 문자열 바꿔서 찾기

--- CodeBasicTranning.playground/Contents.swift
+++ CodeBasicTranning.playground/Contents.swift
@@ -100,3 +100,16 @@ func solution7(_ flo:Double) -> Int {
 print(solution7(1.42))
 print(solution7(69.32))
 print(solution7(69.82))
+
+/*
+ 문자열 바꿔서 찾기
+ 
+ 문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다. myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요.
+*/
+
+func solution8(_ myString:String, _ pat:String) -> Int {
+    return String(myString.map { $0 == "A" ? "B" : "A" }).contains(pat) ? 1 : 0
+}
+
+print(solution8("ABBAA", "AABB"))
+print(solution8("ABAB", "ABAB"))
