c42f08331fa64c200fda436b0e9d8d2a1e138c91
J <knowisdom314@gmail.com>
2026-01-14 13:31:06 +0900

    프로그래머스: 배열의 원소 삭제하기

--- CodeBasicTranning.playground/Contents.swift
+++ CodeBasicTranning.playground/Contents.swift
@@ -127,3 +127,16 @@ func solution9(_ num_list:[Int], _ n:Int) -> Int {
 print(solution9([1, 2, 3, 4, 5], 3)) // 1
 print(solution9([15, 98, 23, 2, 15], 20)) // 0
 
+/*
+ 배열의 원소 삭제하기
+ 
+ 정수 배열 arr과 delete_list가 있습니다. arr의 원소 중 delete_list의 원소를 모두 삭제하고 남은 원소들은 기존의 arr에 있던 순서를 유지한 배열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+func solution10(_ arr:[Int], _ delete_list:[Int]) -> [Int] {
+    return arr.filter { delete_list.contains($0) == false }
+    
+}
+
+print(solution10([293, 1000, 395, 678, 94], [94, 777, 104, 1000, 1, 12])) // [293, 395, 678]
+print(solution10([110, 66, 439, 785, 1], [377, 823, 119, 43])) // [110, 66, 439, 785, 1]
